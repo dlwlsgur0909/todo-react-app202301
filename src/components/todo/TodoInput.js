4850c230fc413e19231ae8fb72e7bb520b252445
dlwlsgur0909 <dlwlsgur0909@naver.com>
2023-01-25 16:08:08 +0900

    # 할 일 조회 등록 서버 요청 처리

--- src/components/todo/TodoInput.js
+++ src/components/todo/TodoInput.js
@@ -3,7 +3,7 @@ import './css/TodoInput.css';
 import { MdAdd } from 'react-icons/md';
 import cn from 'classnames';
 
-const TodoInput = () => {
+const TodoInput = ({add}) => {
 
     // todo-input 박스를 렌더링할지 여부 
     const [open, setOpen] = useState(false);
@@ -13,14 +13,52 @@ const TodoInput = () => {
         setOpen(!open);
     };
 
+    // 입력폼에 입력한 데이터들을 담을 상태 변수 
+    const [todo, setTodo] = useState({
+        title: ''
+    });
+
+    // 입력값을 실시간으로 상태 변수 todo에 저장하는 체인지 이벤트 핸들러
+    const titleChangeHandler = e => {
+        setTodo({
+            // 여러개의 데이터를 받는 다면 spread로 기존의 데이터를 복사하고 값을 바꿔줘야 한다 
+            // ...todo, 
+            title: e.target.value
+        });
+    }
+
+    // 할 일 입력 후 엔터치면 서버로 POST 요청을 보냄 
+    const todoAddHandler = e => {
+        if(e.key === 'Enter') {
+            
+            // 입력 데이터 읽기 
+            console.log(todo);
+
+            // 서버로 요청 보내기 (부모(template)에서 작성한 함수를 받아 매개변수를 넣어준다)
+            add(todo);
+
+            // 입력 끝나면 입력칸 비우기 
+            setTodo({
+                ...todo,
+                title: ''
+            })
+
+        }
+    }
+
+    // submit 방지 핸들러
+    const stopSubmit = e => {
+        e.preventDefault();
+    }
+
  
 
   return (
     <>
         { open && 
             <div className="todo-input">
-                <form className="insert-form">
-                    <input type="text" placeholder="할 일을 입력 후, 엔터를 누르세요!" autoFocus/>
+                <form className="insert-form" onSubmit={stopSubmit}>
+                    <input type="text" placeholder="할 일을 입력 후, 엔터를 누르세요!" autoFocus onKeyUp={todoAddHandler} onChange={titleChangeHandler} value={todo.title}/>
                 </form>
             </div>
         }
--- src/components/todo/TodoTemplate.js
+++ src/components/todo/TodoTemplate.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 // css 로딩
 import './css/TodoTemplate.css';
@@ -8,39 +8,46 @@ import TodoHeader from './TodoHeader';
 import TodoInput from './TodoInput';
 import TodoMain from './TodoMain';
 
-const TodoTemplate = () => {
+const TodoTemplate = (todo) => {
 
+    const API_BASE_URL = 'http://localhost:8080/api/todos';
 
     // 할 일 API 데이터 호출
-    const todos = [
-        {
-            id: 1,
-            title: '아침 산책',
-            done: true
-        },
-        {
-            id: 2,
-            title: '오늘의 뉴스 일기',
-            done: true
-        },
-        {
-            id: 3,
-            title: '샌드위치 사먹기',
-            done: false
-        },
-        {
-            id: 4,
-            title: '리액트 공부하기',
-            done: false
-        }
-    ];
+    const [todos, setTodos] = useState([]);
+
+    // 할 일 등록 서버 요청 
+    const addTodo = (todo) => {
+
+        fetch(API_BASE_URL, {
+            method: 'POST',
+            headers: {
+                'content-type' : 'application/json'
+            },
+            body: JSON.stringify(todo)
+        })
+        .then(res => res.json())
+        .then(result => {
+            setTodos(result.todos);
+        })
+    };
+
+    // 렌더링 되자마자 할 일 => todo API 호출
+    useEffect(() => {
+    
+        fetch(API_BASE_URL)
+        .then(res => res.json())
+        .then(result => {
+            setTodos(result.todos);
+        });
+
+    }, []);
     
   
     return (
     <div className='todo-template'>
         <TodoHeader todoList={todos}/>
         <TodoMain todoList={todos}/>
-        <TodoInput />
+        <TodoInput add={addTodo}/>
     </div>
   )
 }
